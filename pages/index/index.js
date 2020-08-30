4dacd9bd8de334fcc36f2b79592ec495cdc382e2
shangjian <shangjian2019@outlook.com>
2020-08-30 22:32:02 +0800

    小程序项目学习
    首页搜索、轮播图学习

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,38 +1,18 @@
 //Page Object
 Page({
   data: {
-    
+    // 轮播图数组
+    swiperList:[]
   },
-  //options(Object)
+  //页面开始加载是会触发的事件
   onLoad: function(options){
-    
-  },
-  onReady: function(){
-    
-  },
-  onShow: function(){
-    
-  },
-  onHide: function(){
-
-  },
-  onUnload: function(){
-
-  },
-  onPullDownRefresh: function(){
-
-  },
-  onReachBottom: function(){
-
-  },
-  onShareAppMessage: function(){
-
-  },
-  onPageScroll: function(){
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item){
-
+    wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      success: (result)=>{
+        this.setData({
+          swiperList:result.data.message
+        })
+      }
+    });
   }
 });
